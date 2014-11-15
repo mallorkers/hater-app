dc913712634771f59bfe8bd9ada5bb8c9f9faa1d
Miguel Ángel Durán González <conderser@gmail.com>
2014-11-16 00:53:30 +0100

    second version of the interface, added font awesome

--- public/index.php
+++ public/index.php
@@ -5,10 +5,15 @@ require '../3rd_party/vendor/autoload.php';
 
 $slim = new \Slim\Slim();
 
-$slim->get('/test', function(){
-  $loader = new Twig_Loader_Filesystem('../app/templates');
-  $twig = new Twig_Environment($loader);
+$loader = new Twig_Loader_Filesystem('../app/templates');
+$twig = new Twig_Environment($loader);
+
+$slim->get('/test', function() use ($twig){
   echo $twig->render('html.html', array());
+});
+
+$slim->get('/test2', function() use ($twig){
+  echo $twig->render('html2.html', array());
 });
 
 $slim->get('/:name', function($name){
