e6c9157d870f1b7127714338866020a7df24c8d2
Miguel Ángel Durán González <conderser@gmail.com>
2014-12-26 21:33:24 +0100

    asdlkjfhaskljdhfasdf

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
-error_reporting(E_ALL);
+error_reporting(E_ALL); //Change this in production
 ini_set('display_errors', '1');
 require '../3rd_party/vendor/autoload.php';
 
 $slim = new \Slim\Slim();
-echo PHP_VERSION;
+
 $loader = new Twig_Loader_Filesystem('../app/templates');
 $twig = new Twig_Environment($loader);
 
@@ -14,6 +14,10 @@ $slim->get('/test', function() use ($twig){
 
 $slim->get('/test2', function() use ($twig){
   echo $twig->render('html2.html', array());
+});
+
+$slim->get('/env_vars', function(){
+  var_dump(getenv('USER'))."\n";
 });
 
 $slim->get('/:name', function($name){
