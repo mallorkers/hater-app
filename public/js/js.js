1b5ce4dddceb46c87d206f163551b712017ccb61
Miguel Ángel Durán González <conderser@gmail.com>
2014-12-31 21:20:59 +0100

    added animations to login screen

--- public/js/js.js
+++ public/js/js.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
 
+	var opacity1 = { opacity: 1 };
+	var opacity0 = { opacity: 0 };
+
+	var options = {
+		duration: 200,
+		start: function(){ $('.login_screen').removeClass('hidden'); }
+	};
+
+	var options2 = {
+		duration: 200,
+		done: function(){ $('.login_screen').addClass('hidden'); }
+	}
+
 	$('.login_button').on('click', function(){
-		$('.login_screen').removeClass('hidden');
+		$('.login_screen').animate(opacity1, options);
 	});
 
 	$('.login_screen').on('click', function(event){
 		if(event.target.id === 'target')
 		{
-				$('.login_screen').addClass('hidden');
+				$('.login_screen').animate(opacity0, options2);
 		}
 	});
 
